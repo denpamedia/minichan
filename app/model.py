7b73798aa11269b9eceac1c2c291937b8da19d29
A. Stringfield <a.stringfield@yandex.ru>
2014-06-01 23:19:52 +0600

    new design

--- app/model.py
+++ app/model.py
@@ -4,35 +4,32 @@ from mongoengine import *
 class Post(Document):
     post_id = LongField(required=True)
     creation_time = StringField(required=True)
-    body = StringField(max_length=6000, required=True)
-    image_uri = StringField()
-    thumb_uri = StringField()
+    body = StringField(required=True, max_length=6000)
+    subject = StringField()
+    image_id = StringField()
 
     meta = {'allow_inheritance': True}
 
 
 class Thread(Post):
-    last_bump_time = IntField(required=True)
-    bump_counter = IntField(default=0, required=True)
-    bump_limit = BooleanField(default=False, required=True)
-
+    bump_time = FloatField(required=True)
+    bump_counter = IntField(required=True, default=0)
+    bump_limit = BooleanField(required=True, default=False)
     @queryset_manager
     def all(doc_cls, queryset):
-        return [dict(x.to_mongo()) for x in queryset.order_by('-last_bump_time').only('post_id', 'creation_time',
-            'body', 'image_uri', 'thumb_uri', 'last_bump_time', 'bump_counter', 'bump_limit')]
-
+        return [dict(x.to_mongo()) for x in queryset.order_by('-bump_time').only('post_id', 'creation_time',
+            'body', 'image_id', 'subject', 'bump_time', 'bump_counter', 'bump_limit')]
     @queryset_manager
     def oldest(doc_cls, queryset):
-        return queryset.order_by('last_bump_time')[0]
+        return queryset.order_by('bump_time')[0]
 
 
 class Reply(Post):
     thread_link = ReferenceField(Thread, reverse_delete_rule=CASCADE, required=True)
-
     @queryset_manager
     def all(doc_cls, queryset):
         return queryset.order_by('creation_time').only('post_id', 'creation_time',
-            'body', 'image_uri', 'thumb_uri')
+            'body', 'image_id')
 
 
 class Image(Document):
@@ -44,3 +41,8 @@ class Image(Document):
 class Counter(Document):
     name = StringField()
     next_id = IntField(default=0)
+
+
+def next_counter():
+    Counter.objects(name='post_counter').update_one(inc__next_id=1)
+    return Counter.objects[0].next_id
--- app/view.py
+++ app/view.py
@@ -1,28 +1,76 @@
+from time import time
+from datetime import datetime
+import hashlib
+
 from flask import request, redirect, render_template, make_response
+
 from app import minichan
-from app import controller
 from app import model
+from app import config
 
 
 @minichan.route('/', methods=['GET', 'POST'])
 def board():
     if request.method == 'POST':
-        controller.make_new_thread(body=request.form['body'], image=request.files['image'])
+        if model.Thread.objects.count() >= config.NUMBER_OF_THREADS:
+            model.Thread.oldest.delete()
+
+        thread = model.Thread()
+        thread.post_id = next_counter()
+        thread.body = request.form['body']
+        thread.subject = request.form['subject']
+        thread.bump_time = time()
+        thread.creation_time = datetime.now().strftime("%d.%m.%Y %H:%M:%S")
+        thread.save()
+
+        try:
+            image = model.Image(post_link=thread)
+            image.img_src.put(request.files['file'])
+            image.img_id = str(hashlib.md5(image.img_src.read()).hexdigest())
+            image.save()
+            thread.update(set__image_id=image.img_id)
+        except:
+            pass
+
         return redirect('/')
     else:
         return render_template('board.html', data=model.Thread.all)
 
 
-@minichan.route('/<int:thread_id>', methods=['GET', 'POST'])
+@minichan.route('/<thread_id>', methods=['GET', 'POST'])
 def thread(thread_id):
     if request.method == 'POST':
-        body = request.form['body']
-        image = request.files['image']
-        controller.make_new_reply(thread_id=thread_id, body=body, image=image)
+        original_post = model.Thread.objects(post_id=thread_id)[0]
+        original_post.update(inc__bump_counter=1)
+        if original_post.bump_counter >= config.BUMP_LIMIT:
+            original_post.update(set__bump_limit=True)
+        else:
+            original_post.update(set__bump_time=time())
+
+        reply = model.Reply()
+        reply.post_id = next_counter()
+        reply.creation_time = datetime.now().strftime("%d.%m.%Y %H:%M:%S")
+        reply.body = request.form['body']
+        reply.thread_link = original_post
+        reply.save()
+
+        try:
+            image = model.Image(post_link=reply)
+            image.img_src.put(request.files['file'])
+            image.img_id = str(hashlib.md5(image.img_src.read()).hexdigest())
+            image.save()
+            reply.update(set__image_id=image.img_id)
+        except:
+            pass
+
         return redirect('/{0}'.format(thread_id))
     else:
-        data = controller.get_thread(thread_id)
+        data = {
+        "original_post": dict(model.Thread.objects(post_id=thread_id)[0].to_mongo()),
+        "reply_list": [dict(reply.to_mongo()) for reply in model.Reply.all(thread_link=model.Thread.objects(post_id=thread_id)[0])]
+        }
         return render_template('thread.html', data=data)
+        
 
 
 @minichan.route('/<img_type>/<img_id>', methods=['GET'])
@@ -40,3 +88,7 @@ def image(img_type, img_id):
         response.headers['Content-Type'] = 'image/jpeg'
         return response
 
+
+def next_counter():
+    model.Counter.objects(name='post_counter').update_one(inc__next_id=1)
+    return model.Counter.objects[0].next_id
